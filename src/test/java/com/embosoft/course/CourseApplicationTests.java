f8c819ff9748b8d64e296aeb3f138ebc30b4cea4
JoaoVitor83 <vititor666demon@gmail.com>
2023-05-09 17:07:29 -0300

    Adicionando arquivos iniciais do projeto

---
+++ src/test/java/com/embosoft/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embosoft.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
